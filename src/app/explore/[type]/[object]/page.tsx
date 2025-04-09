01aa9fdbbdff5bf055e6b852f23e32f477d86211
dmitrysamochine <micromillion@gmail.com>
2025-04-09 16:49:26 -0400

    cleanup

--- src/app/explore/[type]/[object]/page.tsx
+++ src/app/explore/[type]/[object]/page.tsx
@@ -1,6 +1,5 @@
 "use client"
 
-import { useState, useEffect } from "react"
 import { useParams } from "next/navigation"
 import Header from "@/components/Header"
 import Footer from "@/components/Footer"
--- src/app/explore/[type]/page.tsx
+++ src/app/explore/[type]/page.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import { useTheme } from "@/contexts/ThemeContext"
-import { useState, useEffect } from "react"
 import { useParams } from "next/navigation"
 import Header from "@/components/Header"
 import Footer from "@/components/Footer"
